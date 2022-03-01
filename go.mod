8105d30ed9d3c70e06ed07914882aee0c3a13e0d
hyuoou <63180765+hyuoou@users.noreply.github.com>
2022-03-01 20:53:44 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hyuoou/easy-get-weather
+
+go 1.17
+
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/ramsgoli/Golang-OpenWeatherMap v0.0.0-20200819054846-b9de37dc5370 // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	owm "github.com/ramsgoli/Golang-OpenWeatherMap"
+)
+
+func GetWeatherApi() string {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln(err)
+	}
+	return os.Getenv("WeatherApi")
+}
+
+func GetCity() string {
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		log.Fatalln("引数がありません。引数で都市名を選択してください")
+	}
+	if len(flag.Args()) > 1 {
+		log.Fatalln("引数が大すぎます。都市名のみ選択してください")
+	}
+	return flag.Arg(0)
+}
+
+func ToCelsius(temp float64) float64 {
+	return (temp - 32) / 1.8
+}
+
+func main() {
+	api := owm.OpenWeatherMap{API_KEY: GetWeatherApi()}
+	cityName := GetCity()
+
+	currentWeather, err := api.CurrentWeatherFromCity(cityName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("都市名  :", currentWeather.Name)
+	fmt.Println("天気    :", currentWeather.Weather[0].Main)
+	fmt.Printf("気温    : %.2f°C\n", ToCelsius(currentWeather.Temp))
+	fmt.Printf("湿度    : %d%%\n", currentWeather.Humidity)
+	fmt.Printf("最高気温: %.2f°C\n", ToCelsius(currentWeather.Temp_max))
+	fmt.Printf("最低気温: %.2f°C\n", ToCelsius(currentWeather.Temp_min))
+}
